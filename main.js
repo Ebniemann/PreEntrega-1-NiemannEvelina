54caa514ec6baa7a6a8528813ec63579ac30807b
Evelina Niemann <evelinaniemann@gmail.com>
2023-07-30 20:50:13 -0300

    add localStorage and JSON

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 let carrito = [];
 
-document.addEventListener("DOMContentLoaded", () => {
-  let btn_buy = document.querySelectorAll(".button");
+const key_localstorage_carrito = "carrito";
 
+document.addEventListener("DOMContentLoaded", () => {
+  carrito = JSON.parse(localStorage.getItem(key_localstorage_carrito)) || [];
+
+  const btn_buy = document.querySelectorAll(".button");
   btn_buy.forEach((boton, index) => {
+    const btnIndex = index;
     boton.addEventListener("click", (e) => {
-      e.preventDefault();
-      const index = Array.from(btn_buy).indexOf(boton);
-      const producto = curso[index];
-      addCarrito(producto);
+      //const index = Array.from(btn_buy).indexOf(boton);
+      const producto = curso[btnIndex];
+      agregarAlCarrito(producto);
     });
   });
+  actualizarCarrito();
 });
