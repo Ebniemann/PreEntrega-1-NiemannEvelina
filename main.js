7d42255f240822f6f54af9eb5f00ff1ebd2828d9
Evelina Niemann <evelinaniemann@gmail.com>
2023-07-31 00:05:08 -0300

    Fix html

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const key_localstorage_carrito = "carrito";
 document.addEventListener("DOMContentLoaded", () => {
   carrito = JSON.parse(localStorage.getItem(key_localstorage_carrito)) || [];
 
-  const btn_buy = document.querySelectorAll(".button");
+  const btn_buy = document.querySelectorAll(".btnDelete");
   btn_buy.forEach((boton, index) => {
     const btnIndex = index;
     boton.addEventListener("click", (e) => {
