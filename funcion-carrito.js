70894cc12fffbf62d75eb523125037e64b5b77ed
Evelina Niemann <evelinaniemann@gmail.com>
2023-08-08 20:03:07 -0300

    Add function seleccionarCurso and styles

--- funcion-carrito.js
+++ funcion-carrito.js
@@ -1,165 +1,251 @@
-curso = [
-  {
-    id: 1,
-    nombre: "Product Web",
-    precio: 10000,
-    cantidad: 1,
-  },
-  {
-    id: 2,
-    nombre: "Product",
-    precio: 12000,
-    cantidad: 1,
-  },
-  {
-    id: 3,
-    nombre: "Desarrollo de aplicaciones",
-    precio: 14000,
-    cantidad: 1,
-  },
-  {
-    id: 4,
-    nombre: "Desarrollo Full Stack",
-    precio: 16000,
-    cantidad: 1,
-  },
-  {
-    id: 5,
-    nombre: "Desarrollo UX/UI",
-    precio: 18000,
-    cantidad: 1,
-  },
-  {
-    id: 6,
-    nombre: "Product Design",
-    precio: 20000,
-    cantidad: 1,
-  },
-];
-
-function agregarAlCarrito(item) {
-  let productoExiste = carrito.some((prod) => prod.id === item.id);
-
-  if (productoExiste) {
-    Toastify({
-      text: "Este curso ya esta en tu carrito",
-      duration: 2000,
-      gravity: "bottom",
-      style: {
-        background: "linear-gradient(to right, #EC3A3A, #FAAC58)",
+class Compra {
+  iniciarCompra() {
+    this.curso = [
+      {
+        id: 1,
+        nombre: "Product Web",
+        precio: 1000,
+        cantidad: 1,
+        cantidad_cursos: 3,
+        lista_cursos: ["Product Manager", "Diseño UX/UI", "Desarrollo Web"],
+        cantidad_semanas: 31,
       },
-    }).showToast();
-  } else {
-    carrito.push(item);
-
-    Toastify({
-      text: "Curso agregado con exito!",
-      duration: 2000,
-      gravity: "bottom",
-      style: {
-        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      {
+        id: 2,
+        nombre: "Product",
+        precio: 2000,
+        cantidad: 1,
+        cantidad_cursos: 2,
+        lista_cursos: ["Product Manager", "E-commerce"],
+        cantidad_semanas: 16,
       },
-    }).showToast();
+      {
+        id: 3,
+        nombre: "Desarrollo de aplicaciones",
+        precio: 3000,
+        cantidad: 1,
+        cantidad_cursos: 4,
+        lista_cursos: [
+          "Desarrollo Web",
+          "JavaScript",
+          "React Js",
+          "Desarrollo de Aplicaciones",
+        ],
+        cantidad_semanas: 35,
+      },
+      {
+        id: 4,
+        nombre: "Desarrollo Full Stack",
+        precio: 4000,
+        cantidad: 1,
+        cantidad_cursos: 4,
+        lista_cursos: [
+          "Desarrollo Web",
+          "JavaScript",
+          "React Js",
+          "Programación Backend",
+        ],
+        cantidad_semanas: 50,
+      },
+    ];
+
+    this.cargarCurso();
   }
-  actualizarCarrito();
-}
 
-function eliminarProducto(item) {
-  Swal.fire({
-    title: "Desea eliminar el curso?",
-    showCancelButton: true,
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Elimninar",
-    cancelButtonText: "Cancelar",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      carrito = carrito.filter((articulo) => articulo.id != item.id);
-      this.actualizarCarrito();
+  //Funcion que crea las card de los cursos.
+  cargarCurso() {
+    let cardNueva = document.getElementById("careers");
+    cardNueva.innerHTML = "";
 
-      //notidico de la eliminacion
+    this.curso.forEach((cursoCard) => {
+      const {
+        nombre,
+        precio,
+        id,
+        cantidad_cursos,
+        lista_cursos,
+        cantidad_semanas,
+      } = cursoCard;
+
+      let card = document.createElement("div");
+      card.classList.add("box-cart");
+      card.id = "div_" + id;
+      card.innerHTML = `
+                  
+                  <h3>${nombre}</h3>
+                  <h5>Incluye ${cantidad_cursos} cursos</h5>
+                  <ul>
+                  ${lista_cursos.map((curso) => `<li>${curso}</li>`).join("")}
+                  </ul>
+                  <div>
+                  <p>${cantidad_semanas} semanas / 2 clase semanales</p>
+                  <p class="precio">$ ${precio}</p>
+             
+                  `;
+      let seleccionarCursoBtn = document.createElement("a");
+      seleccionarCursoBtn.textContent = "Agregar al carrito";
+
+      seleccionarCursoBtn.addEventListener("click", () => {
+        agregarCarrito(id);
+      });
+
+      card.appendChild(seleccionarCursoBtn);
+      cardNueva.appendChild(card);
+    });
+  }
+
+  // Berifico si el curso no esta en el carrito y lo agrego.
+  seleccionarCurso(item) {
+    let productoExiste = carrito.some((prod) => prod.id === item.id);
+
+    if (productoExiste) {
       Toastify({
-        text: "Producto eliminado con exito",
+        text: "Este curso ya esta en tu carrito",
         duration: 2000,
         gravity: "bottom",
+        style: {
+          background: "linear-gradient(to right, #EC3A3A, #FAAC58)",
+        },
+      }).showToast();
+    } else {
+      carrito.push(item);
+
+      Toastify({
+        text: "Curso agregado con exito!",
+        duration: 2000,
+        gravity: "bottom",
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
       }).showToast();
     }
-  });
-}
+    this.actualizarCarrito();
+  }
 
-function actualizarCarrito() {
-  actualizarContador();
-  mostrarCarrito();
-  guardarCarrito();
-}
+  //Eliminar cursos del carrito
+  eliminarProducto(item) {
+    Swal.fire({
+      title: "Desea eliminar este curso?",
+      showCancelButton: true,
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Elimninar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        carrito = carrito.filter((articulo) => articulo.id != item.id);
+        this.actualizarCarrito();
 
-function actualizarContador() {
-  let total = carrito.reduce(
-    (acumulador, carrito) => acumulador + carrito.cantidad,
-    0
-  );
+        Toastify({
+          text: "Producto eliminado con exito",
+          duration: 1500,
+          gravity: "bottom",
+        }).showToast();
+      }
+    });
+  }
 
-  let contador = document.getElementById("compra");
-  contador.innerHTML = total;
-}
+  actualizarCarrito() {
+    this.actualizarContador();
+    this.mostrarCarrito();
+    this.guardarCarrito();
+  }
 
-function mostrarCarrito() {
-  let detalle = document.getElementById("buy-cart");
-  let totalCarrito = 0;
-  detalle.innerHTML = "";
+  actualizarContador() {
+    let total = carrito.reduce(
+      (acumulador, carrito) => acumulador + carrito.cantidad,
+      0
+    );
 
-  let header = document.createElement("div");
-  header.classList.add("general-cart");
+    let contador = document.getElementById("compra");
+    contador.innerHTML = total;
+  }
 
-  let titleCurso = document.createElement("h3");
-  titleCurso.textContent = "Mi carrito:";
+  //Mostrar el detalle de los cursos en el carrito
+  mostrarCarrito() {
+    let detalle = document.getElementById("buy-cart");
+    let totalCarrito = 0;
+    detalle.innerHTML = "";
 
-  header.appendChild(titleCurso);
-  detalle.appendChild(header);
+    let header = document.createElement("div");
+    header.classList.add("general-cart");
 
-  carrito.forEach((productCart) => {
-    const { nombre, precio, id } = productCart;
+    let titleCurso = document.createElement("h3");
+    titleCurso.textContent = "Mi carrito:";
 
-    totalCarrito += parseInt(precio);
+    header.appendChild(titleCurso);
+    detalle.appendChild(header);
 
-    let descripcion = document.createElement("div");
-    descripcion.id = "div" + id;
-    descripcion.classList.add("general-cart");
+    carrito.forEach((productCart) => {
+      const { nombre, precio, id } = productCart;
 
-    let identificador = document.createElement("p");
-    identificador.textContent = nombre;
+      totalCarrito += parseInt(precio);
 
-    let valor = document.createElement("p");
-    valor.textContent = "$" + precio;
+      let descripcion = document.createElement("div");
+      descripcion.id = "div" + id;
+      descripcion.classList.add("general-cart");
 
-    let eliminar = document.createElement("div");
+      let identificador = document.createElement("p");
+      identificador.textContent = nombre;
 
-    let eliminarBtn = document.createElement("a");
-    eliminarBtn.classList.add("btnEliminar");
-    eliminarBtn.innerHTML = `
+      let valor = document.createElement("p");
+      valor.textContent = precio;
+
+      let eliminarBtn = document.createElement("a");
+      eliminarBtn.classList.add("btnEliminar");
+      eliminarBtn.innerHTML = `
                           
                           <p>🗑️</p>
                           
                           `;
 
-    eliminarBtn.addEventListener("click", () => {
-      eliminarProducto(productCart);
+      eliminarBtn.addEventListener("click", () => {
+        this.eliminarProducto(productCart);
+      });
+
+      descripcion.appendChild(identificador);
+      descripcion.appendChild(valor);
+      descripcion.appendChild(eliminarBtn);
+      detalle.appendChild(descripcion);
     });
-    eliminar.appendChild(eliminarBtn);
 
-    descripcion.appendChild(identificador);
-    descripcion.appendChild(valor);
-    descripcion.appendChild(eliminar);
-    detalle.appendChild(descripcion);
-  });
+    let total = document.createElement("p");
+    total.textContent += `Total: $${totalCarrito}`;
 
-  let total = document.createElement("p");
-  total.classList.add("total");
-  total.textContent += `Total: $${totalCarrito}`;
+    detalle.appendChild(total);
 
-  detalle.appendChild(total);
-}
+    let pagarBtn = document.createElement("button");
+    pagarBtn.textContent = "Realizar pago";
 
-function guardarCarrito() {
-  const carritoJSON = JSON.stringify(carrito);
-  localStorage.setItem(key_localstorage_carrito, carritoJSON);
+    pagarBtn.addEventListener("click", () => {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Compra exitosa!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      this.limpiarCarrito();
+      this.vaciarCarrito();
+    });
+
+    detalle.appendChild(pagarBtn);
+  }
+
+  // Guardar carrito en localStorage.
+  guardarCarrito() {
+    const carritoJSON = JSON.stringify(carrito);
+    localStorage.setItem(key_localstorage_carrito, carritoJSON);
+  }
+
+  //Vaciar el carrito.
+  vaciarCarrito() {
+    carrito = [];
+    this.actualizarContador();
+    this.mostrarCarrito();
+  }
+
+  //Limpiar el localStorage del carrito.
+  limpiarCarrito() {
+    localStorage.clear();
+  }
 }
--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 let carrito = [];
+let nueva_compra;
 
 const key_localstorage_carrito = "EVELINA_NIEMANN_CARRITO";
 
 document.addEventListener("DOMContentLoaded", () => {
   carrito = JSON.parse(localStorage.getItem(key_localstorage_carrito)) || [];
 
-  const btn_buy = document.querySelectorAll(".buttonAdd");
-  btn_buy.forEach((boton, index) => {
-    const btnIndex = index;
-    boton.addEventListener("click", (e) => {
-      //const index = Array.from(btn_buy).indexOf(boton);
-      const producto = curso[btnIndex];
-      agregarAlCarrito(producto);
-    });
-  });
-  actualizarCarrito();
+  nueva_compra = new Compra();
+
+  nueva_compra.iniciarCompra();
+  nueva_compra.actualizarCarrito();
 });
+
+function agregarCarrito(id) {
+  const cursos = document.querySelector("#div_" + id);
+
+  let nombre = cursos.querySelector("h3").textContent;
+  let precioString = cursos.querySelector(".precio").textContent;
+  let precio = parseInt(precioString.substring(2));
+
+  let cursoAdquirido = new Cursos(id, nombre, precio);
+
+  nueva_compra.seleccionarCurso(cursoAdquirido);
+}
