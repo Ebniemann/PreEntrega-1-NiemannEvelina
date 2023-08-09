9de3a538297541cf22f0862402fe0ac7778e88e6
Evelina Niemann <evelinaniemann@gmail.com>
2023-08-09 19:07:19 -0300

    Change Formulario.js

--- formulario.js
+++ formulario.js
@@ -1,54 +1,68 @@
 let new_usuario = new Array();
 
-let validar = document.getElementById("valido");
-let check = document.getElementById("checked");
+let nameE = document.getElementById("input_name");
+let emailE = document.getElementById("input_email");
+let textE = document.getElementById("input_text");
 
 let btn_form = document.getElementById("btn-form");
 btn_form.addEventListener("click", () => {
-  console.log("cick");
-
   validarForm();
 });
 
+//valida que los campor del form este completos
 function validarForm() {
-  validar.innerHTML = "";
-  check.innerHTML = "";
-
+  nameE.textContent = "";
+  emailE.textContent = "";
+  textE.textContent = "";
   let input_nombre = document.getElementById("name").value;
   let input_email = document.getElementById("email").value;
   let input_textArea = document.getElementById("text-area").value;
 
-  let mensaje = new Array();
-
   if (!input_nombre) {
-    mensaje.push("Ingrese su nombre");
+    let error_name = document.createElement("p");
+    error_name.innerHTML = "Ingrese su nombre";
+    nameE.appendChild(error_name);
   }
   if (!input_email) {
-    mensaje.push("Ingrese su email");
+    let error_email = document.createElement("p");
+    error_email.textContent = "Ingrese su email";
+    emailE.appendChild(error_email);
+  } else if (!validarEmail(input_email)) {
+    let validar_email = document.createElement("p");
+    validar_email.textContent = "Ingrese un email válido";
+    emailE.appendChild(validar_email);
   }
   if (!input_textArea) {
-    mensaje.push("Ingrese su consulta");
-  }
-  if (mensaje.length > 0) {
-    let lista = document.createElement("ul");
-    lista.textContent = "Tiene que cargar los siguientes datos: ";
-
-    mensaje.forEach((e) => {
-      lista.appendChild(crear_li(e));
-    });
-    validar.appendChild(lista);
+    let error_text = document.createElement("p");
+    error_text.textContent = "Ingrese su consulta";
+    textE.appendChild(error_text);
   } else {
-    let checked = document.createElement("p");
-    checked.textContent = "Su consulta fue enviada con éxito";
-
-    check.appendChild(checked);
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Su consulta fue enviada!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    setTimeout(() => {
+      limpiarForm();
+    }, 1000);
   }
-
-  return mensaje.length == 0;
 }
 
-function crear_li(mensaje) {
-  let li = document.createElement("li");
-  li.textContent = mensaje;
-  return li;
+//valida el formato del input email
+function validarEmail(email) {
+  const validarEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+  let esValido = validarEmail.test(email);
+
+  if (esValido) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//limpia el form una vez que se envia correctamente
+function limpiarForm() {
+  document.getElementById("contacto").reset();
 }
